7e77333db4cbf2d676c9e475af43d6e2e80b6885
devapp <admin@devapp.pro>
2017-03-14 10:05:48 +0300

    fix user session

--- components/auth.js
+++ components/auth.js
@@ -12,13 +12,23 @@ exports = module.exports = function () {
     return function(req, res, next) {
 
         if (req.session.user != undefined) {
-            var user = new UserModel(req.session.user);
-            console.log(user.role);
-            if(user.can([UserModel.roles.manager, UserModel.roles.admin, UserModel.roles.dev])){
-                return next();
-            }
+            UserModel
+                .findOne({_id: req.session.user._id})
+                .then(function (us) {
+                    if(
+                        us.can([UserModel.roles.manager, UserModel.roles.admin, UserModel.roles.dev]) &&
+                        us.status == UserModel.statuses.Active
+                    ){
+                        return next();
+                    } else {
+                        return res.redirect(config.get('urls:admin_login'));
+                    }
+                })
+                .catch(function (err) {
+                    return res.redirect(config.get('urls:admin_login'));
+                });
+        } else {
+            return res.redirect(config.get('urls:admin_login'));
         }
-        return res.redirect(config.get('urls:admin_login'));
-
     };
 };
